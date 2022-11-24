03706882b2d944cd895bfff6f7e2503aadf9e0a7
nefcanto <sn.1361@gmail.com>
2022-11-24 07:55:39 +0330

    m

--- components/Hero.js
+++ components/Hero.js
@@ -1,8 +1,21 @@
 const Hero = () => {
     return <div
+        className="relative mx-4"
     >
         <div className="relative h-screen">
             <div className="bg-[#ff9900] w-[300px] h-[300px] opacity-70 rounded-full blur-[100px]"></div>
+
+        </div>
+        <div className="absolute top-32 text-white font-inter">
+            <div className="text-4xl font-extrabold flex flex-col gap-3">
+                <div>Big Ideas.</div>
+                <div>Creative People.</div>
+                <div>New Technology.</div>
+            </div>
+            <div className="text-lg my-6 leading-8 ">A full-service digital marketing firm that specialises in human-centered experiences. We bring companies and people together.</div>
+            <div>CTA</div>
+            <div>Boasting</div>
+            <img src="/images/hero.png" />
         </div>
     </div>
 }
