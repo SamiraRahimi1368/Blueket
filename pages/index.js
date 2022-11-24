86c74d3a180d033e215310c6f763f18779cfb2eb
nefcanto <sn.1361@gmail.com>
2022-11-24 06:38:01 +0330

    committed by script

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,7 @@
-import Menu from "../components/Menu"
-import Hero from "../components/Hero"
-import Services from "../components/Services"
-import About from "../components/About"
 export default function Home() {
   return (
     <>
-      <Menu/>
-      <Hero/>
-      <Services/>
-      <About/>
-     
+      hi
     </>
-    /*
-    menu
-    hero
-    services
-    footer
-    */
   )
 }
