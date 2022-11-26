0279323849a14e1fe8f61a3c4dc5ff717c041eb1
Samira <samira.rahimi1998@gmail.com>
2022-11-25 21:44:23 -0500

    my-update

---
+++ components/Services.js
@@ -0,0 +1,193 @@
+const Services=()=>{
+    <head>
+<script src="https://kit.fontawesome.com/yourcode.js" crossorigin="anonymous"></script>
+
+</head>
+    return <div className="relative" id="services">
+        <div className="absoulte text-white flex justify-center text-center text-3xl font-bold font-inter">
+Services We Provide        
+        </div>
+        <div className="pr-20 pt-5 absoulte text-white flex justify-center text-center text-1xl  font-inter">
+    
+        Blueket overcomes challenges, achieves results, and adds value to our clients and partners. 
+        Take a look at some of our clients' success stories.
+        </div>
+
+        <div class=" text-white grid grid-cols-2  mt-20  ">
+           
+        <div className="ml-6  mr-10 flex justify-center rounded-lg bg-zinc-800 	border-gray-300">
+            <div><img src="/images/android-service.jpg" className=" rounded-xl h-50 w-80 p-3 "/>
+            <div className="text-white flex flex-col text-1xl  font-inter">
+                <ul className=" p-5 pt-2 text-gray-400 	">
+                App Development
+                <svg className="h-8 w-10 text-white-500"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="3"  stroke-linecap="round"  stroke-linejoin="round">  <line x1="5" y1="12" x2="19" y2="12" /></svg>
+                <li className="p-5 text-white list-dic flex ">
+                    <svg className="h-3 w-2 text-white-500 mt-2 mr-2"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <circle cx="12" cy="12" r="10" /></svg>
+                    Ios Development
+
+                </li>
+                <li className="pt-1 p-5  text-white list-none flex ">
+                <svg class="h-3 w-2 text-white-500 mt-2 mr-2"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <circle cx="12" cy="12" r="10" /></svg>
+
+                    Flutter Development
+                </li>
+                <li className="pt-1 p-5  text-white list-none flex">
+                <svg class="h-3 w-2 text-white-500 mt-2 mr-2"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <circle cx="12" cy="12" r="10" /></svg>
+
+                    Cross Platfrom Development
+                </li>
+                <li className="pt-1 p-5  text-white list-none flex">
+                <svg class="h-3 w-2 text-white-500 mt-2 mr-2"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <circle cx="12" cy="12" r="10" /></svg>
+
+                    React Native Development 
+                </li>
+                <li className="pt-1 p-5  text-white list-none flex">
+                <svg class="h-3 w-2 text-white-500 mt-2 mr-2"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <circle cx="12" cy="12" r="10" /></svg>
+
+                    On demand App solution
+                </li>
+
+                </ul>
+            </div>
+            
+               
+
+            </div>
+        </div>
+  
+        <div className="ml-6  mr-10 flex justify-center rounded-lg bg-zinc-800 	border-gray-300">
+            <div><img src="/images/webdesign.jpg" className=" 	h-50 w-80 p-3 rounded"/>
+            <div className="text-white flex flex-col text-1xl  font-inter">
+                <ul className="p-5 pt-2 text-gray-400  	">
+                Web Development
+                <svg className="h-8 w-10 text-white-500"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="3"  stroke-linecap="round"  stroke-linejoin="round">  <line x1="5" y1="12" x2="19" y2="12" /></svg>
+
+                <li className="p-5 text-white list-none flex ">
+                <svg className="h-3 w-2 text-white-500 mt-2 mr-2"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <circle cx="12" cy="12" r="10" /></svg>
+
+                    PHP Development
+                    <i className="fas fa-band-aid"></i>
+                </li>
+                <li className="pt-1 p-5  text-white list-none flex">
+                <svg className="h-3 w-2 text-white-500 mt-2 mr-2"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <circle cx="12" cy="12" r="10" /></svg>
+
+                    Node development
+                </li>
+                <li className="pt-1 p-5  text-white list-none flex">
+                <svg className="h-3 w-2 text-white-500 mt-2 mr-2"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <circle cx="12" cy="12" r="10" /></svg>
+
+                    Angular Development
+                </li>
+                <li className="pt-1 p-5  text-white list-none flex">
+                <svg className="h-3 w-2 text-white-500 mt-2 mr-2"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <circle cx="12" cy="12" r="10" /></svg>
+
+                   Python Development 
+                </li>
+                <li className="pt-1 p-5  text-white list-none flex">
+                <svg className="h-3 w-2 text-white-500 mt-2 mr-2"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <circle cx="12" cy="12" r="10" /></svg>
+
+                    Codelgniter Development
+                </li>
+                
+                </ul>
+            </div>
+            
+               
+
+            </div>
+        </div>
+</div>
+<div class=" text-white grid grid-cols-2  mt-20  ">
+           
+           <div className="ml-6  mr-10 flex justify-center rounded-lg bg-zinc-800 	border-gray-300">
+               <div><img src="/images/digitalmarketing.jpg" className=" rounded-md h-50 w-80 p-3 "/>
+               <div className="text-white flex flex-col text-1xl  font-inter">
+                   <ul className="p-5 pt-2 text-gray-400  	">
+                   Digital Marketing
+                   <svg className="h-8 w-10 text-white-500"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="3"  stroke-linecap="round"  stroke-linejoin="round">  <line x1="5" y1="12" x2="19" y2="12" /></svg>
+
+                   <li className="p-5 text-white list-none flex">
+                   <svg className="h-3 w-2 text-white-500 mt-2 mr-2"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <circle cx="12" cy="12" r="10" /></svg>
+
+                       Ios Development
+                   </li>
+                   <li className="pt-1 p-5  text-white list-none flex">
+                   <svg className="h-3 w-2 text-white-500 mt-2 mr-2"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <circle cx="12" cy="12" r="10" /></svg>
+
+                       Flutter Development
+                   </li>
+                   <li className="pt-1 p-5  text-white list-none flex">
+                   <svg className="h-3 w-2 text-white-500 mt-2 mr-2"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <circle cx="12" cy="12" r="10" /></svg>
+
+                       Cross Platfrom Development
+                   </li>
+                   <li className="pt-1 p-5  text-white list-none flex">
+                   <svg className="h-3 w-2 text-white-500 mt-2 mr-2"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <circle cx="12" cy="12" r="10" /></svg>
+
+                       React Native Development 
+                   </li>
+                   <li className="pt-1 p-5  text-white list-none flex">
+                   <svg className="h-3 w-2 text-white-500 mt-2 mr-2"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <circle cx="12" cy="12" r="10" /></svg>
+
+                       On demand App solution
+                   </li>
+   
+                   </ul>
+               </div>
+               
+                  
+   
+               </div>
+           </div>
+     
+           <div className="ml-6  mr-10 flex justify-center rounded-lg bg-zinc-800 	border-gray-300 delay-75	 ">
+               <div><img src="/images/ecommercestore.jpg" className=" rounded h-50 w-80 p-3 "/>
+               <div className="text-white flex flex-col text-1xl  font-inter">
+                   <ul className="p-5 pt-2 text-gray-400">
+                  <svg className="h-8 w-10 text-white-500"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="3"  stroke-linecap="round"  stroke-linejoin="round">  <line x1="5" y1="12" x2="19" y2="12" /></svg>
+
+                   <li className="p-5 text-white list-none flex">
+
+                   <svg className="h-3 w-2 text-white-500 mt-2 mr-2"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <circle cx="12" cy="12" r="10" /></svg>
+
+                       PHP Development
+                   </li>
+                   <li className="pt-1 p-5  text-white list-none flex">
+                   <svg className="h-3 w-2 text-white-500 mt-2 mr-2"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <circle cx="12" cy="12" r="10" /></svg>
+
+                       Node development
+                   </li>
+                   <li className="pt-1 p-5  text-white list-none flex">
+                   <svg className="h-3 w-2 text-white-500 mt-2 mr-2"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <circle cx="12" cy="12" r="10" /></svg>
+
+                       Angular Development
+                   </li>
+                   <li className="pt-1 p-5  text-white list-none flex">
+                   <svg className="h-3 w-2 text-white-500 mt-2 mr-2"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <circle cx="12" cy="12" r="10" /></svg>
+
+                      Python Development 
+                   </li>
+                   <li className="pt-1 p-5  text-white list-none flex">
+                   <svg className="h-3 w-2 text-white-500 mt-2 mr-2"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <circle cx="12" cy="12" r="10" /></svg>
+
+                       Codelgniter Development
+                   </li>
+                   
+                   </ul>
+               </div>
+               
+                  
+   
+               </div>
+           </div>
+   </div>
+   
+   
+   
+
+
+
+          </div>
+}
+
+export default Services
