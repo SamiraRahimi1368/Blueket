cb7253ac4d29a01e6ab8f83d8277ceafa121ddc0
Samira <samira.rahimi1998@gmail.com>
2022-12-08 20:26:23 -0500

    UserPage Updated & Products pages Updated

--- pages/users.js
+++ pages/users.js
@@ -1,19 +1,16 @@
 const Users = ({ users }) => {
 
-    // const total = props.total
-    // const products = props.products
-
-    // const { total, products } = props
+  
 
     return <div
-        className="text-white grid mx-auto max-w-screen-md grid-cols-3 gap-x-6 gap-y-20 mt-5 border-white"
+        className="text-white grid mx-auto max-w-screen-md grid-cols-3 gap-x-6 gap-y-20 mt-5 "
     >
         {
             users.map(users => <div
                 key={users.id}
-                className="border-solid border-white"
+                className="border border-orange-300	rounded-lg p-5 	"
             >
-                <h3 className="text-white ">Name: {users.firstName} {users.lastName}</h3>
+                <h3 className="text-white">Name: {users.firstName} {users.lastName}</h3>
                 <h3 className="text-white ">Age : {users.age}</h3>
                 <h3 className="text-white ">Email: {users.email}</h3>
                 <h3 className="text-white ">Phone: {users.phone}</h3>
