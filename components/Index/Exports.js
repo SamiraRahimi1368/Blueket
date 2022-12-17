31def3b94a0e3f08b85eecb24d431785354898a7
Samira <samira.rahimi1998@gmail.com>
2022-12-17 13:52:59 -0500

    export removed

--- components/Index/Exports.js
+++ components/Index/Exports.js
@@ -1,9 +1,5 @@
 import Hero from "./Hero";
 import Sections from './Sections'
-import Services from './Services'
-import Service from "./Service";
 
 export { Hero }
 export { Sections }
-export { Services }
-export { Service }
